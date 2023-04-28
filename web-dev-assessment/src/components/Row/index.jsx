36757c34e29b6f764edb568153afd66f84cf4449
KantKontrol <nick.derissio.tech@gmail.com>
2023-04-28 02:16:09 -0400

    chore: updated row with new components

--- web-dev-assessment/src/components/Row/index.jsx
+++ web-dev-assessment/src/components/Row/index.jsx
@@ -1,74 +1,85 @@
 import React from "react";
+import ExampleInput from "../ExampleInput";
+import ExampleDropdown from "../ExampleDropdown";
 
-const Row = ({ pointNumber, onDataUpdate, disabled }) => {
+const Row = ({
+  pointNumber,
+  name,
+  displayType,
+  offset,
+  manualValue,
+  displayText,
+  temp,
+  humd,
+  manual,
+  onFieldUpdate,
+  disabled,
+}) => {
   return (
     <tr style={{ backgroundColor: disabled ? "yellow" : "gray" }}>
       <td>{pointNumber}</td>
       <td>
-        <input
+        <ExampleInput
           name="name"
-          value="test"
-          onChange={({ target }) =>
-            onDataUpdate(pointNumber, "name", target.value)
-          }
+          value={name}
+          onFieldUpdate={onFieldUpdate}
+          pointNumber={pointNumber}
+          disabled={disabled}
         />
       </td>
       <td>
-        <select>
-          <option value="exampleoption1" selected>
-            Example option 1
-          </option>
-          <option value="exampleoption2">Example option 2</option>
-          <option value="exampleoption3">Example option 3</option>
-        </select>
+        <ExampleDropdown
+          name="displayType"
+          value={displayType}
+          onFieldUpdate={onFieldUpdate}
+          pointNumber={pointNumber}
+          disabled={disabled}
+        />
       </td>
       <td>
-        <input
+        <ExampleInput
           name="offset"
-          value="test"
-          onChange={({ target }) =>
-            onDataUpdate(pointNumber, "offset", target.value)
-          }
+          value={offset}
+          onFieldUpdate={onFieldUpdate}
+          pointNumber={pointNumber}
+          disabled={disabled}
         />
       </td>
       <td>
-        <input
-          name="manualval"
-          value="test"
-          onChange={({ target }) =>
-            onDataUpdate(pointNumber, "manualval", target.value)
-          }
+        <ExampleInput
+          name="manualValue"
+          value={manualValue}
+          onFieldUpdate={onFieldUpdate}
+          pointNumber={pointNumber}
+          disabled={disabled}
         />
       </td>
       <td>
-        <select name="displaytext">
-          <option value="notused" selected>
-            Not used
-          </option>
-          <option value="exampleoption1">Example option 1</option>
-          <option value="exampleoption2">Example option 2</option>
-          <option value="exampleoption3">Example option 3</option>
-        </select>
+        <ExampleDropdown
+          name="displayText"
+          value={displayText}
+          onFieldUpdate={onFieldUpdate}
+          pointNumber={pointNumber}
+          disabled={disabled}
+        />
       </td>
       <td>
-        <select name="temp">
-          <option value="notused" selected>
-            Not used
-          </option>
-          <option value="exampleoption1">Example option 1</option>
-          <option value="exampleoption2">Example option 2</option>
-          <option value="exampleoption3">Example option 3</option>
-        </select>
+        <ExampleDropdown
+          name="temp"
+          value={temp}
+          onFieldUpdate={onFieldUpdate}
+          pointNumber={pointNumber}
+          disabled={disabled}
+        />
       </td>
       <td>
-        <select name="humidity">
-          <option value="notused" selected>
-            Not used
-          </option>
-          <option value="exampleoption1">Example option 1</option>
-          <option value="exampleoption2">Example option 2</option>
-          <option value="exampleoption3">Example option 3</option>
-        </select>
+        <ExampleDropdown
+          name="humd"
+          value={humd}
+          onFieldUpdate={onFieldUpdate}
+          pointNumber={pointNumber}
+          disabled={disabled}
+        />
       </td>
       <td>toggle</td>
     </tr>
