17421f14810e2e9e5443e85dc7ea6e373f22ebd3
KantKontrol <nick.derissio.tech@gmail.com>
2023-04-28 01:15:50 -0400

    chore: created basic row component

---
+++ web-dev-assessment/src/components/Row/index.jsx
@@ -0,0 +1,78 @@
+import React from "react";
+
+const Row = ({ pointNumber, onDataUpdate, disabled }) => {
+  return (
+    <tr>
+      <td>{pointNumber}</td>
+      <td>
+        <input
+          name="name"
+          value="test"
+          onChange={({ target }) =>
+            onDataUpdate(pointNumber, "name", target.value)
+          }
+        />
+      </td>
+      <td>
+        <select>
+          <option value="exampleoption1" selected>
+            Example option 1
+          </option>
+          <option value="exampleoption2">Example option 2</option>
+          <option value="exampleoption3">Example option 3</option>
+        </select>
+      </td>
+      <td>
+        <input
+          name="offset"
+          value="test"
+          onChange={({ target }) =>
+            onDataUpdate(pointNumber, "offset", target.value)
+          }
+        />
+      </td>
+      <td>
+        <input
+          name="manualval"
+          value="test"
+          onChange={({ target }) =>
+            onDataUpdate(pointNumber, "manualval", target.value)
+          }
+        />
+      </td>
+      <td>
+        <select name="displaytext">
+          <option value="notused" selected>
+            Not used
+          </option>
+          <option value="exampleoption1">Example option 1</option>
+          <option value="exampleoption2">Example option 2</option>
+          <option value="exampleoption3">Example option 3</option>
+        </select>
+      </td>
+      <td>
+        <select name="temp">
+          <option value="notused" selected>
+            Not used
+          </option>
+          <option value="exampleoption1">Example option 1</option>
+          <option value="exampleoption2">Example option 2</option>
+          <option value="exampleoption3">Example option 3</option>
+        </select>
+      </td>
+      <td>
+        <select name="humidity">
+          <option value="notused" selected>
+            Not used
+          </option>
+          <option value="exampleoption1">Example option 1</option>
+          <option value="exampleoption2">Example option 2</option>
+          <option value="exampleoption3">Example option 3</option>
+        </select>
+      </td>
+      <td>toggle</td>
+    </tr>
+  );
+};
+
+export default Row;
