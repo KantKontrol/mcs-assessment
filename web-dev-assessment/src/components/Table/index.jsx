26e869ce826081e5da656d36fc22e0eb77a84c9a
KantKontrol <nick.derissio.tech@gmail.com>
2023-04-30 18:15:11 -0400

    chore: updated table style and global style

--- web-dev-assessment/src/components/Table/index.jsx
+++ web-dev-assessment/src/components/Table/index.jsx
@@ -3,6 +3,19 @@ import React from "react";
 const Table = ({ headers, ...props }) => {
   return (
     <table style={{ borderSpacing: "5px 5px" }}>
+      <caption style={{ backgroundColor: "lightgray", height: "40px" }}>
+        <div
+          style={{
+            width: "100%",
+            height: "100%",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <h2>Sensor Input Information Screen</h2>
+        </div>
+      </caption>
       <thead>
         <tr>
           {headers.map((header) => (
