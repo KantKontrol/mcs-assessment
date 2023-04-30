987d2ecf1aae6217d19bc50bd601ce9de349d7dd
KantKontrol <nick.derissio.tech@gmail.com>
2023-04-30 18:14:34 -0400

    chore: updated counter style

--- web-dev-assessment/src/components/SICounter/index.jsx
+++ web-dev-assessment/src/components/SICounter/index.jsx
@@ -3,14 +3,30 @@ import React from "react";
 
 const SICounter = ({ sensorAmount = 0, adjustSensoramount }) => {
   return (
-    <div>
-      <span>Total number of SI's: </span>
-      <span style={{ fontWeight: "bold" }}>{sensorAmount}</span>
-      <button onClick={() => adjustSensoramount(adjustSensor.INCREASE)}>
-        Increase
+    <div
+      style={{
+        width: "300px",
+        display: "flex",
+        padding: "1px",
+        backgroundColor: "lightgray",
+        border: "1px solid black",
+      }}
+    >
+      <span style={{ flex: 3 }}>Total number of SI's: </span>
+      <span style={{ fontWeight: "bold", flex: 1, textAlign: "center" }}>
+        {sensorAmount}
+      </span>
+      <button
+        style={{ flex: 1, fontWeight: "bolder" }}
+        onClick={() => adjustSensoramount(adjustSensor.DECREASE)}
+      >
+        {"<"}
       </button>
-      <button onClick={() => adjustSensoramount(adjustSensor.DECREASE)}>
-        Decrease
+      <button
+        style={{ flex: 1, fontWeight: "bolder" }}
+        onClick={() => adjustSensoramount(adjustSensor.INCREASE)}
+      >
+        {">"}
       </button>
     </div>
   );
