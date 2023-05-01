a8f4d628ab279456ca3663f2c4ce7616756d537e
KantKontrol <nick.derissio.tech@gmail.com>
2023-05-01 15:57:01 -0400

    java: created base class for test

---
+++ test.java
@@ -0,0 +1,18 @@
+
+
+
+public class test {
+
+
+	public int[] findIndices (int[] nums, int target){
+
+		for(int i =0;i<nums.length;i++){
+
+		}
+
+	}
+
+	public static void main(String[] args){
+		System.out.println("test!");
+	}
+}
